4519c416489f868c960ec46dc47788e6dd5ba640
RobertoNDH <robertonauzet@gmail.com>
2026-02-25 23:43:46 +0000

    add: implement init function to fetch and display cat data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import './style.css';
+import { fetchCats } from "./api.js";
+
+const init = async () => {
+    console.log("Solicitando datos de gatos...");
+
+    const cats = await fetchCats(9);
+
+    if (cats.length > 0) {
+        console.log("Datos de gatos recibidos!");
+        console.table(cats);
+    } else {
+        console.warn("No se recibieron datos de gatos.");
+    }
+}
+
+init();
